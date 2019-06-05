ce6d0e00aab92fa597670db8f81bded29e17ce7b
Akai <yujinkai@tianlankeji.com>
2019-06-05 17:56:23 +0800

    yjk

--- cod-admin/src/main/java/com/tlkj/cod/admin/service/impl/CodAdminSendEmailServiceImpl.java
+++ cod-admin/src/main/java/com/tlkj/cod/admin/service/impl/CodAdminSendEmailServiceImpl.java
@@ -1,15 +1,19 @@
 package com.tlkj.cod.admin.service.impl;
 
 import com.sun.mail.util.MailSSLSocketFactory;
-import com.tlkj.cod.admin.service.SendEmailService;
+import com.tlkj.cod.admin.service.CodAdminSendEmailService;
 import org.springframework.stereotype.Service;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.Date;
@@ -22,7 +26,7 @@ import java.util.Properties;
  * @Date 2019/6/4 4:59 PM
  */
 @Service
-public class SendEmailServiceImpl implements SendEmailService {
+public class CodAdminSendEmailServiceImpl implements CodAdminSendEmailService {
 
     /**
      * 邮件服务器主机名
@@ -49,10 +53,10 @@ public class SendEmailServiceImpl implements SendEmailService {
      * @return
      */
     @Override
-    public boolean sendEmail(String toEmailAddress, String emailTitle, String emailContent) {
+    public boolean sendEmail(String toEmailAddress, String emailTitle, String emailContent, String filename) {
         Properties props = new Properties();
         // 开启debug调试
-        props.setProperty("mail.debug", "true");
+        //props.setProperty("mail.debug", "true");
         // 发送服务器需要身份验证
         props.setProperty("mail.smtp.auth", "true");
         // 端口号
@@ -84,9 +88,13 @@ public class SendEmailServiceImpl implements SendEmailService {
             msg.setSentDate(new Date());
             //设置邮件内容
             msg.setText(builder.toString());
+            // 设置要发送附件的文件路径
+//            DataSource source = new FileDataSource(filename);
+//            msg.setDataHandler(new DataHandler(source));
+//            msg.setFileName(MimeUtility.encodeText(filename));
             //设置发件人邮箱
             // InternetAddress 的三个参数分别为: 发件人邮箱, 显示的昵称(只用于显示, 没有特别的要求), 昵称的字符集编码
-            msg.setFrom(new InternetAddress(myEmailAccount,"测试发送人", "UTF-8"));
+            msg.setFrom(new InternetAddress(myEmailAccount));
             //得到邮差对象
             Transport transport = session.getTransport();
             //连接自己的邮箱账户
@@ -94,11 +102,11 @@ public class SendEmailServiceImpl implements SendEmailService {
             transport.connect(myEmailSMTPHost, myEmailAccount, myEmailPassword);
             //发送邮件
             transport.sendMessage(msg, new Address[]{new InternetAddress(toEmailAddress)});
-            //将该邮件保存到本地
-            OutputStream out = new FileOutputStream("MyEmail.eml");
-            msg.writeTo(out);
-            out.flush();
-            out.close();
+//            //将该邮件保存到本地
+//            OutputStream out = new FileOutputStream("MyEmail.eml");
+//            msg.writeTo(out);
+//            out.flush();
+//            out.close();
             transport.close();
         } catch (Exception e) {
             e.printStackTrace();
