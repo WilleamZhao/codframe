82d84fb3f6ae377047f2ae260524a9a3549c6a24
sourcod <zhaochunjie@sourcod.com>
2019-06-22 22:32:58 +0800

    add video ffmpeg

--- cod-video/src/main/java/com/tlkj/cod/video/InitVideo.java
+++ cod-video/src/main/java/com/tlkj/cod/video/InitVideo.java
@@ -1,12 +1,29 @@
 package com.tlkj.cod.video;
 
+import com.tlkj.cod.launcher.CodModuleInitialize;
+import com.tlkj.cod.launcher.model.CodModuleLauncherModel;
+
 /**
- * Desc
+ * Desc 初始化视频模块
  *
  * @author sourcod
  * @version 1.0
  * @className InitVideo
  * @date 2019/6/19 12:56 PM
  */
-public class InitVideo {
+public class InitVideo implements CodModuleInitialize {
+    @Override
+    public int order() {
+        return 0;
+    }
+
+    @Override
+    public void init(CodModuleLauncherModel codModuleLauncherModel) {
+
+    }
+
+    @Override
+    public void fail(Throwable e) {
+
+    }
 }
