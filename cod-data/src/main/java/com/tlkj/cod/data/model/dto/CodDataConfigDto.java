a7ad410890450faa4753032a9be349e94564abb8
sourcod <zhaochunjie@sourcod.com>
2019-08-13 17:10:51 +0800

    update

---
+++ cod-data/src/main/java/com/tlkj/cod/data/model/dto/CodDataConfigDto.java
@@ -0,0 +1,12 @@
+package com.tlkj.cod.data.model.dto;
+
+/**
+ * Desc
+ *
+ * @author sourcod
+ * @version 1.0
+ * @className CodDataConfigDto
+ * @date 2019/8/13 4:51 PM
+ */
+public class CodDataConfigDto {
+}
